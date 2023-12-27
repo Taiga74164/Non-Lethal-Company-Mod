ad488028e4f3c620ddc2eb88f34b3f29f72a7b18
KingRainbow44 <kobedo11@gmail.com>
2023-12-27 16:22:44 -0500

    (feat:modules) Introduce module system

---
+++ NonLethalCompany-Mod/Module.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx.Logging;
+
+namespace NonLethalCompany_Mod.Modules;
+
+public abstract class Module
+{
+    public ILogSource Logger => NonLethalCompany.Instance.ModLogger;
+
+    /// <summary>
+    /// Called when the module is loaded.
+    /// </summary>
+    public abstract void Load();
+}
---
+++ NonLethalCompany-Mod/ModuleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace NonLethalCompany_Mod.Modules;
+
+public static class ModuleManager
+{
+    /// <summary>
+    /// Loads all modules in the 'NonLethalCompany_Mod' namespace.
+    /// </summary>
+    public static void LoadAllModules()
+    {
+        var types = Assembly.GetExecutingAssembly().GetTypes();
+        foreach (var type in types)
+        {
+            if (type.Namespace != "NonLethalCompany_Mod.Modules") continue;
+            if (type.BaseType != typeof(Module)) continue;
+
+            var method = type.GetMethod("Load");
+            method?.Invoke(null, null);
+        }
+    }
+}
